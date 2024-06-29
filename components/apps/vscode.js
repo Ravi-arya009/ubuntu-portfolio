06ba5147e36b4b70d837e37fb8b3543b672f81fd
raviarya <ravi.arya009@gmail.com>
2024-06-29 11:54:53 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   components/apps/vscode.js

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function VsCode() {
     return (
-        <iframe src="https://github1s.com/ravi9arya/ravi9arya.github.io/blob/HEAD/components/ubuntu.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
-        // this is not my work, but it's amazing!
-        // Here is the link to the original repo: https://github.com/conwnet/github1s
-    )
+      <iframe src="https://github1s.com/Ravi-arya009/ubuntu-portfolio/blob/main/components/ubuntu.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
+      // this is not my work, but it's amazing!
+      // Here is the link to the original repo: https://github.com/conwnet/github1s
+    );
 }
 
 export const displayVsCode = () => {
